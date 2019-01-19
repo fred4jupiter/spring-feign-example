fac1977450334a9857c71beca906f9bd02208133
fred4jupiter <hamsterhase@gmx.de>
2019-01-19 14:41:01 +0100

    update

--- feign-example-client/src/test/java/com/fred4jupiter/feignexample/client/CustomerClientMT.java
+++ feign-example-client/src/test/java/com/fred4jupiter/feignexample/client/CustomerClientMT.java
@@ -1,27 +1,33 @@
 package com.fred4jupiter.feignexample.client;
 
 import com.fred4jupiter.feignexample.api.Customer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
-@Service
-public class CallService {
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerClientMT {
 
-    private static final Logger LOG = LoggerFactory.getLogger(CallService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(CustomerClientMT.class);
 
     @Autowired
     private CustomerClient customerClient;
 
-    @PostConstruct
-    public void printCustomers() {
+    @Test
+    public void findAllCustomers() {
         List<Customer> allCustomers = customerClient.findAllCustomers();
         LOG.info("allCustomers={}", allCustomers);
+    }
 
+    @Test
+    public void findCustomersByName() {
         List<Customer> customers = customerClient.findCustomersByName("A");
         LOG.info("customers={}", customers);
     }
