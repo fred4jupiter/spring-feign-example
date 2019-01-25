a10e32497c34ef321e2cadc002e2f7257fdf1f6f
Staehler, Michael <michael.staehler@opitz-consulting.com>
2019-01-25 11:53:04 +0100

    externalized server config

--- feign-example-client/src/main/java/com/fred4jupiter/feignexample/client/CustomerClient.java
+++ feign-example-client/src/main/java/com/fred4jupiter/feignexample/client/CustomerClient.java
@@ -3,7 +3,7 @@ package com.fred4jupiter.feignexample.client;
 import com.fred4jupiter.feignexample.api.CustomerServiceApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "customerClient", url = "http://localhost:8080")
+@FeignClient("customerClient")
 public interface CustomerClient extends CustomerServiceApi {
 
 }
