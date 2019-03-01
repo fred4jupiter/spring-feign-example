7952e3e0221b381d1390239c7629a43581dc79cc
Staehler, Michael <michael.staehler@opitz-consulting.com>
2019-03-01 09:07:18 +0100

    setting url explicitly in config

--- feign-example-client/src/main/java/com/fred4jupiter/feignexample/client/CustomerClient.java
+++ feign-example-client/src/main/java/com/fred4jupiter/feignexample/client/CustomerClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 import java.util.Map;
 
-@FeignClient("customerClient")
+@FeignClient(name = "customerClient", url = "${my.server.api.url}")
 public interface CustomerClient {
 
     @GetMapping("/customers")
